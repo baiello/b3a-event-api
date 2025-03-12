c9393da8d5c6e0b908019f263d3306aef6e991a6
Brice AIELLO <brice.aiello.fr@gmail.com>
2025-03-12 13:47:04 +0100

    Add show one event details endpoint

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const prisma = new PrismaClient();
 
 app.use(express.json()) // for parsing application/json
 
+// Endpoint to create one event
 app.post('/events', async (req, res) => {
     try {
         const { title, description, date } = req.body;
@@ -30,10 +31,30 @@ app.post('/events', async (req, res) => {
     }
 });
 
+// Endpoint to list all events
 app.get('/events', async (req, res) => {
     try {
         const events = await prisma.event.findMany();
         return res.status(200).json(events);
+    } catch (error) {
+        return res.status(500).json({ "error": error.message});
+    }
+});
+
+// Endpoint to show one event details
+app.get('/events/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const event = await prisma.event.findUnique({
+            where: {
+                id: parseInt(id),
+            }
+        });
+
+        return (!event)
+            ? res.status(404).json({ "error": "Resource not found" })
+            : res.status(200).json(event);
     } catch (error) {
         return res.status(500).json({ "error": error.message});
     }
