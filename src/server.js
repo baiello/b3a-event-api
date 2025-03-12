87e3e4dcaf8fe12c623667946344a1281124d20e
Brice AIELLO <brice.aiello.fr@gmail.com>
2025-03-12 12:25:10 +0100

    Add event creation endpoint

--- src/server.js
+++ src/server.js
@@ -1,11 +1,33 @@
 const express = require("express");
+const { PrismaClient } = require("@prisma/client")
 
 const PORT = 3000;
 
 const app = express();
+const prisma = new PrismaClient();
 
-app.get('/', (req, res) => {
-    res.send('Server is running');
+app.use(express.json()) // for parsing application/json
+
+app.post('/events', async (req, res) => {
+    try {
+        const { title, description, date } = req.body;
+
+        if (!title || !description || !date) {
+            throw new Error("Input is missing");
+        }
+
+        const event = await prisma.event.create({
+            data: {
+                title: title,
+                description: description,
+                date: (new Date(date)).toISOString(),
+            }
+        });
+
+        return res.status(201).json(event);
+    } catch (error) {
+        return res.status(500).json({ "error": error.message});
+    }
 });
 
 app.listen(PORT, () => {
